219ae5809edca8ce0cbf01d7fe3deae66d8fd015
Pavlov Kostyantyn <realanka77@gmail.com>
2023-07-08 18:36:54 +0300

    feat: add properly closed server

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import { httpServer } from "./src/http_server/index";
-import { createWebsocketServer } from "./src/ws_server";
-
+import "./src/ws_server";
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
-
-createWebsocketServer();
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,21 +1,32 @@
+import { httpServer } from "../../src/http_server";
 import { MyWebSocket, parseMsg } from "../../src/websocketHandlers/parseMsg";
-import { WebSocket, WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
 
-export function createWebsocketServer() {
-  // After starting the program displays websocket parameters - which one?
-  // вывести адресс и порт на консоль
-  const wss: WebSocketServer = new WebSocketServer(
-    {
-      port: 3000,
-    },
-    () => console.log("Websocket server is running on ", wss.address())
-  );
+const wss: WebSocketServer = new WebSocketServer(
+  {
+    port: 3000,
+  },
+  () => console.log("Websocket server is running on ", wss.address())
+);
 
-  wss.on("connection", (ws: MyWebSocket) => {
-    ws.on("error", console.error);
+wss.on("connection", (ws: MyWebSocket) => {
+  ws.on("error", console.error);
 
-    ws.on("message", (msg) => {
-      parseMsg(msg, ws);
-    });
+  ws.on("message", (msg) => {
+    parseMsg(msg, ws);
   });
-}
+});
+
+process.on("SIGINT", () => {
+  console.log("\nAll connection and sockets will closed");
+  wss.clients.forEach((socket) => {
+    socket.send("server_was_closed");
+    socket.close();
+  });
+  wss.close(() => {
+    httpServer.close(() => process.exit(0));
+  });
+});
+
+httpServer.on("close", () => console.log("Http server is closed"));
+wss.on("close", () => console.log("Tcp server is closed"));
